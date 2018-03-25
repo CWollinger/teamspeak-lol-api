e60359e967365e8a6f05477b19add42714b61072
CWollinger <CWollinger@web.de>
2018-03-25 17:38:13 +0200

    Update lolstatus.sh

--- lolstatus.sh
+++ lolstatus.sh
@@ -2,6 +2,16 @@
 ### Version: 1.0
 ### Author: Christian Wollinger
 ############### Description ###############
+LOL_USER=`grep LOL_USER env.txt`
+LOL_PORT=`grep LOL_PORT env.txt`
+LOL_PASSWORD=`grep LOL_PASSWORD env.txt`
+LOL_NICK=`grep LOL_NICK env.txt`
+LOL_IP=`grep LOL_IP env.txt`
+eval $LOL_USER
+eval $LOL_PORT
+eval $LOL_PASSWORD
+eval $LOL_NICK
+eval $LOL_IP
 
 > lolstatus.tmp
 curl -s http://status.leagueoflegends.com/shards/euw | jq '.' | sed -n -e '/Game/{p;n;n;p;}' | sed 's/"//g' | sed 's/,/[\/color]/g' | awk '{print $2}' | sed ':a;N;$!ba;s/\n/ -> [color=green]/g' | sed 's/Game/[color=blue]Game/g' >> lolstatus.tmp
